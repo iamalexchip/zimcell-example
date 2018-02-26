c881d50090512748b0df2a1fc24a0a04de0c11dd
zerochip <semhiles@gmail.com>
2018-02-26 21:32:07 +0200

    brand link

--- index.php
+++ index.php
@@ -24,6 +24,7 @@ if (isset($_POST['provider']))
 	$provider = $_POST['provider'];
 }
 
+/**************************/
 ?>
 <!DOCTYPE html>
 <html>
@@ -64,7 +65,7 @@ if (isset($_POST['provider']))
               <span class="icon-bar"></span>
               <span class="icon-bar"></span>
             </button>
-            <a class="navbar-brand" href="#" style="font-size: 22px; margin-top: 5px">Zimcell Demo</a>
+            <a class="navbar-brand" href="/" style="font-size: 22px; margin-top: 5px">Zimcell Demo</a>
           </div>
           <div id="navbar" class="navbar-collapse collapse">
             <ul class="nav navbar-nav navbar-right">
